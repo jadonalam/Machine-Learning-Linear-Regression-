b5da9bd90504a7b3f9f263379607c41c7e702f37
jadonalam <77247796+jadonalam@users.noreply.github.com>
2021-04-12 20:25:03 -0700

    Add files via upload

---
+++ Regression_Working_File.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+import pickle
+from matplotlib import style
+from yellowbrick import regressor
+
+data = pd.read_csv('student2/student-mat.csv', sep=';')
+#print(data)
+#print(data.head())
+
+data = data[["G1","G2","G3","studytime","failures","absences"]]
+
+#print(data)
+predict = "G3"
+
+X = np.array(data.drop([predict],1))
+y = np.array(data[predict])
+
+x_train, x_test , y_train , y_test = sklearn.model_selection.train_test_split(X,y,test_size=0.1)
+
+model = linear_model.LinearRegression()
+
+model.fit(x_train,y_train)
+acc = model.score(x_test,y_test)
+
+pred = model.predict(x_test)
+
+print(acc)
+
+regressor.residuals_plot(model, x_train,y_train,x_test,y_test)
+
+print('pred: ', pred, 'actual: ', y_test)
+
+"""
+best=0
+for _ in range(100):
+
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+    
+    linear = linear_model.LinearRegression()
+
+    linear.fit(x_train,y_train)
+    acc = linear.score(x_test, y_test)
+    print(acc)
+
+    if acc > best:
+        best = acc
+        with open("studentmodel.pickle","wb") as f:
+            pickle.dump(linear, f)"""
+
+# pickle_in = open("studentmodel.pickle","rb")
+# linear = pickle.load(pickle_in)
+# linear.fit(x_train,y_train)
+# acc = linear.score(x_test, y_test)
+# print(acc)
+
+# print("Co: \n",  linear.coef_)
+# print("Intercept: \n", linear.intercept_)
+
+# predictions = linear.predict(x_test)
+
+# for x in range(len(predictions)):
+#     print(predictions[x],x_test[x],y_test[x])
+
+# p = "G1"
+# style.use("ggplot")
+# plt.scatter(data[p], data["G3"])
+# plt.xlabel(p)
+# plt.ylabel("Final Grade")
+# plt.show()
